b4516f13d7a9636012e5d9ae389abc2211cd37f6
Zjtep <zhu-johnny@hotmail.com>
2017-12-01 01:22:31 -0500

    anchor complete

--- module/RS.py
+++ module/RS.py
@@ -22,7 +22,7 @@ def getFullPosition():
     # cv2.imwrite('temp.png', img)
 
 
-    img_rgb = cv2.imread(r'C:\Users\PPC\git\RS_BOT_2.0\library\reference\full_screen_02.jpg')
+    img_rgb = cv2.imread(r'C:\Users\PPC\git\RS_BOT_2.0\library\reference\full_screen_04.jpg')
     # img_rgb = cv2.imread(r'C:\Users\PPC\git\RS_BOT_2.0\library\reference\full_window2.JPG')
 
     # img_rgb = cv2.imread(r'C:\Users\PPC\git\RS_BOT_2.0\module\temp.png')
@@ -75,52 +75,125 @@ def getItemPosition(win_coord):
     item = []
 
     x1 = win_coord[0]
-
     y1 = win_coord[1]
-    y2 = win_coord[1]+ item_size
-    for x in range(4):
+    x2 = win_coord[0]+item_size
+    y2 = win_coord[1]+item_size
 
-        offset_x = item_size + (x*item_size)
+    for m in range(7):
+        for n in range(4):
+            # print n
 
-        x2 = win_coord[0] + offset_x+(x*spacing)
-        if x > 0:
-            # x2 = win_coord[0] + offset_x + (x * spacing)
-            x2 = win_coord[0] + offset_x +(x*spacing)
+            item.append([x1, y1, x2, y2])
 
-
-
-
-        item.append([x1, y1, x2, y2])
-        # x1 += offset_x + spacing
-
-
-
-
-        # y1 += y1+offset_x
-        # x2 += offset_x
-        # y2 +=y2+offset_x
-
-        # x1 = win_coord[0] +offset_x
-        # y1 = win_coord[1]
-
-    # for x in range(4):
-    #     for y in range(7):
-    #         x1 =win_coord[0]+x*item_size
-    #         y1 = win_coord[1]+x*item_size
-    #
-    #         x2 =(win_coord[0]+x*item_size)+item_size
-    #         y2 =(win_coord[0]+x*item_size)+item_size
-    #
-    #         item.append([x1,y1,x2,y2])
-
+            x1 += item_size
+            x2 += item_size
+            x1 +=spacing
+            x2 +=spacing
+        x1 = win_coord[0]
+        x2 = win_coord[0]+item_size
+        y1 += item_size
+        y2 += item_size
 
     return item
+def getMapPosition(win_coord):
+    off_set = [573, 41]
+    window_size = [144, 151]
+
+    start_coord = [win_coord[0] + off_set[0], win_coord[1] + off_set[1]]
+    return [start_coord[0], start_coord[1], start_coord[0] + window_size[0], start_coord[1] + window_size[1]]
+
+def getHpPosition(win_coord):
+    off_set = [523, 88]
+    window_size = [22, 15]
+
+    start_coord = [win_coord[0] + off_set[0], win_coord[1] + off_set[1]]
+    return [start_coord[0], start_coord[1], start_coord[0] + window_size[0], start_coord[1] + window_size[1]]
+
+def getPrayerPosition(win_coord):
+    off_set = [524, 122]
+    window_size = [22, 15]
+
+    start_coord = [win_coord[0] + off_set[0], win_coord[1] + off_set[1]]
+    return [start_coord[0], start_coord[1], start_coord[0] + window_size[0], start_coord[1] + window_size[1]]
+
+def getRunPosition(win_coord):
+    off_set = [536, 154]
+    window_size = [22, 15]
+
+    start_coord = [win_coord[0] + off_set[0], win_coord[1] + off_set[1]]
+    return [start_coord[0], start_coord[1], start_coord[0] + window_size[0], start_coord[1] + window_size[1]]
+
+def getSpecPosition(win_coord):
+    off_set = [566, 179]
+    window_size = [22, 15]
+
+    start_coord = [win_coord[0] + off_set[0], win_coord[1] + off_set[1]]
+    return [start_coord[0], start_coord[1], start_coord[0] + window_size[0], start_coord[1] + window_size[1]]
+
+def getMenuPosition(win_coord):
+    off_set = [530, 201]
+    item_size = [33,36]
+
+    start_coord = [win_coord[0] + off_set[0], win_coord[1] + off_set[1]]
+
+    item = []
+    x1 = start_coord[0]
+    y1 = start_coord[1]
+    x2 = start_coord[0] + item_size[0]
+    y2 = start_coord[1] + item_size[1]
+
+    for m in range(7):
+        item.append([x1, y1, x2, y2])
+
+        x1 += item_size[0]
+        x2 += item_size[0]
 
 
-img_rgb = cv2.imread(r'C:\Users\PPC\git\RS_BOT_2.0\library\reference\full_screen_02.jpg')
+    off_set = [530, 499]
+    start_coord = [win_coord[0] + off_set[0], win_coord[1] + off_set[1]]
+
+    x1 = start_coord[0]
+    y1 = start_coord[1]
+    x2 = start_coord[0] + item_size[0]
+    y2 = start_coord[1] + item_size[1]
+
+    for m in range(7):
+        item.append([x1, y1, x2, y2])
+
+        x1 += item_size[0]
+        x2 += item_size[0]
+
+    print item
+    return item
+
+    # x1 = win_coord[0]
+    # y1 = win_coord[1]
+    # y2 = win_coord[1]+ item_size
+    # for x in range(4):
+    #
+    #     offset_x = item_size + (x*item_size)
+    #
+    #     x2 = win_coord[0] + offset_x+(x*spacing)
+    #     if x > 0:
+    #         # x2 = win_coord[0] + offset_x + (x * spacing)
+    #         x2 = win_coord[0] + offset_x +(x*spacing)
+    #
+    #
+    #
+    #
+    #     item.append([x1, y1, x2, y2])
+    #     # x1 += offset_x + spacing
+
+
+
+
+
+
+
+img_rgb = cv2.imread(r'C:\Users\PPC\git\RS_BOT_2.0\library\reference\full_screen_04.jpg')
 
 pt = getFullPosition()
-cv2.rectangle(img_rgb, (pt[0], pt[1]), (pt[2], pt[3]), (0, 255, 100), 1)
+cv2.rectangle(img_rgb, (pt[0], pt[1]), (pt[2], pt[3]), (0, 255, 100), 3)
 
 pt2 = getGamePosition(pt)
 cv2.rectangle(img_rgb, (pt2[0], pt2[1]), (pt2[2], pt2[3]), (0, 255, 100), 1)
@@ -137,10 +210,28 @@ pt5 = getInventoryPosition(pt)
 temp_list = getItemPosition(pt5)
 
 for temp in temp_list:
-    print temp
+    # print temp
     # print temp[0]
     cv2.rectangle(img_rgb, (temp[0], temp[1]), (temp[2], temp[3]), (0, 255, 100), 1)
 
+temp_list2 = getMenuPosition(pt)
+for temp2 in temp_list2:
+    cv2.rectangle(img_rgb, (temp2[0], temp2[1]), (temp2[2], temp2[3]), (0, 255, 100), 1)
+
+pt5 = getMapPosition(pt)
+cv2.rectangle(img_rgb, (pt5[0], pt5[1]), (pt5[2], pt5[3]), (0, 255, 100), 1)
+
+pt6 = getHpPosition(pt)
+cv2.rectangle(img_rgb, (pt6[0], pt6[1]), (pt6[2], pt6[3]), (0, 255, 100), 1)
+
+pt7 = getPrayerPosition(pt)
+cv2.rectangle(img_rgb, (pt7[0], pt7[1]), (pt7[2], pt7[3]), (0, 255, 100), 1)
+
+pt8 = getRunPosition(pt)
+cv2.rectangle(img_rgb, (pt8[0], pt8[1]), (pt8[2], pt8[3]), (0, 255, 100), 1)
+
+pt9 = getSpecPosition(pt)
+cv2.rectangle(img_rgb, (pt9[0], pt9[1]), (pt9[2], pt9[3]), (0, 255, 100), 1)
 
 
 cv2.imwrite('asdf.png', img_rgb)
